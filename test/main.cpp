49b6be564cbe627b3e5e27239b6f81d55fbdaa02
WangRunji <wangrunji0408@163.com>
2017-10-16 10:38:03 +0800

    Add GoogleTest

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 王润基 on 2017/10/16.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 王润基 on 2017/10/16.
+//
+
+#include <gtest/gtest.h>
+
+TEST(SampleTestSuit, SampleTest)
+{
+    EXPECT_EQ(2, 1+1);
+    EXPECT_EQ(6, 8-2);
+}
