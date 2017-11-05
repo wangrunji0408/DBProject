ba47bc3041b389f55ca47d961b3dc0fbe1c2452f
王润基 <wrj15@mails.tsinghua.edu.cn>
2017-11-05 12:56:08 +0800

    Update official dataset

--- dataset_small/create.sql
+++ dataset_small/create.sql
@@ -1,46 +1,46 @@
-CREATE DATABASE orderDB_small;
+CREATE DATABASE orderDB;
 
-USE orderDB_small;
+USE orderDB;
 
 CREATE TABLE customer(
-	id int(10) NOT NULL,
-	name varchar(25) NOT NULL,
-	gender char(1) NOT NULL,
+	id INT(10) NOT NULL,
+	name VARCHAR(25) NOT NULL,
+	gender VARCHAR(1) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE book (
-  id int(10) NOT NULL,
-  title varchar(100) NOT NULL,
-  authors varchar(200),
-  publisher varchar(100),
-  copies int(10),
+  id INT(10) NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  authors VARCHAR(200),
+  publisher VARCHAR(100),
+  copies INT(10),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE website(
-	id int(10) NOT NULL,
-	name varchar(25) NOT NULL,
-	url varchar(100),
+	id INT(10) NOT NULL,
+	name VARCHAR(25) NOT NULL,
+	url VARCHAR(100),
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE price(
-	website_id int(10) NOT NULL,
-	book_id int(10) NOT NULL,
-	price float NOT NULL,
+	website_id INT(10) NOT NULL,
+	book_id INT(10) NOT NULL,
+	price FLOAT NOT NULL,
 	PRIMARY KEY (website_id,book_id),
 	FOREIGN KEY (website_id) REFERENCES website(id),
 	FOREIGN KEY (book_id) REFERENCES book(id)
 );
 
 CREATE TABLE orders(
-	id int(10) NOT NULL,
-	website_id int(10) NOT NULL,
-	customer_id int(10) NOT NULL,
-	book_id int(10) NOT NULL,
-	date date,
-	quantity int(10),
+	id INT(10) NOT NULL,
+	website_id INT(10) NOT NULL,
+	customer_id INT(10) NOT NULL,
+	book_id INT(10) NOT NULL,
+	date DATE,
+	quantity INT(10),
 	PRIMARY KEY (id),
 	FOREIGN KEY (website_id) REFERENCES website(id),
 	FOREIGN KEY (customer_id) REFERENCES customer(id),
