0fb67f87f632ad33b6acfecb12ee7d7a0576c01d
Ben Pig Chu <benpichu@gmail.com>
2017-12-18 15:55:16 +0800

    output parsing error

--- src/driver/InteractiveDriver.cpp
+++ src/driver/InteractiveDriver.cpp
@@ -4,13 +4,19 @@
 #include "driver/SQLExecutor.h"
 #include "driver/InteractiveDriver.h"
 #include "parser/Parser.h"
+#include "ast/Exceptions.h"
 
 void InteractiveDriver::start(){
 	while(!(input.eof()||input.fail()||input.bad())){
 		::std::string line;
 		::std::getline(input,line);
 		if(line.back()!='\\'){
-			executer.executeSQL(Parser::parseString(buffer+line),output);
+			::std::vector<::std::unique_ptr<Statement>> program;
+			try{
+				executer.executeSQL(Parser::parseString(buffer+line),output);
+			}catch(const ParseError& error){
+				output<<"Error when parsing command:\n"<<error.what()<<::std::endl;
+			}
 			buffer.clear();
 		}else{
 			line.pop_back();
